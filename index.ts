5590c6d2b3b1f508f9391757138d117a37dee318
Tim Untersberger <timuntersberger2@gmail.com>
2019-03-22 16:10:25 +0100

    .:

---
+++ index.ts
@@ -0,0 +1,109 @@
+import Webuntis from "./webuntis"
+
+function intToDate(x) {
+    const str = new String(x)
+    const year = str.substr(0, 4)
+    const month = str.substr(4, 2)
+    const day = str.substr(6, 2)
+    return new Date(parseInt(year), parseInt(month), parseInt(day))
+}
+
+async function main() {
+    const webuntis = new Webuntis(
+        "htbla linz leonding",
+        "if150152",
+        "teamtengu1",
+        "mese.webuntis.com"
+    )
+    await webuntis.login()
+    const absences = await webuntis.getAbsences(20180910, 20190707)
+    const subjects = await webuntis.getSubjects()
+    const allLessons = await webuntis.getTimetableForRange(20180910, 20190707)
+    const subject_count = {}
+    const missedSubject_count = {}
+
+    allLessons.forEach(lesson => {
+        if (lesson.code || !lesson.su[0]) return
+
+        const subjectId = lesson.su[0].id
+
+        if (!subject_count[subjectId]) subject_count[subjectId] = 0
+
+        subject_count[subjectId]++
+    })
+
+    await Promise.all(
+        absences.map(async absence => {
+            const {
+                startDate,
+                endDate,
+                startTime,
+                endTime,
+                isExcused
+            } = absence
+
+            if (startDate === endDate) {
+                const lessons = await webuntis.getTimetableAt(startDate)
+
+                lessons.forEach(lesson => {
+                    if (
+                        lesson.startTime >= startTime &&
+                        lesson.endTime <= endTime &&
+                        !lesson.code
+                    ) {
+                        const missedSubjectId = lesson.su[0].id
+
+                        if (!missedSubject_count[missedSubjectId])
+                            missedSubject_count[missedSubjectId] = 0
+
+                        missedSubject_count[missedSubjectId]++
+                    }
+                })
+            }
+            //handle absence longer than 1 day
+            else {
+                const lessons = await webuntis.getTimetableForRange(absence.startDate, absence.endDate)
+
+                lessons.forEach(lesson => {
+                    if (
+                        lesson.startTime >= startTime &&
+                        lesson.endTime <= endTime &&
+                        !lesson.code
+                    ) {
+                        const missedSubjectId = lesson.su[0].id
+
+                        if (!missedSubject_count[missedSubjectId])
+                            missedSubject_count[missedSubjectId] = 0
+
+                        missedSubject_count[missedSubjectId]++
+                    }
+                })
+            }
+        })
+    )
+
+    console.log(
+        `You missed a total of ${Object.keys(missedSubject_count)
+            .map(key => missedSubject_count[key])
+            .reduce((x, y) => x + y)} hours`
+    )
+
+    Object.keys(missedSubject_count)
+        .map(key => ({
+            subject: subjects.filter(subject => subject.id == key)[0],
+            count: missedSubject_count[key],
+            percentage: Math.round(
+                (missedSubject_count[key] / subject_count[key]) * 100
+            )
+        }))
+        .sort((a, b) => b.count - a.count)
+        .forEach(info =>
+            console.log(
+                `You missed ${info.count} hours of '${
+                    info.subject.name
+                }' that is ${info.percentage}%`
+            )
+        )
+}
+
+main()
---
+++ webuntis.ts
@@ -0,0 +1,178 @@
+import Axios, { AxiosAdapter, AxiosInstance } from "axios"
+import * as CookieBuilder from "cookie"
+
+export interface WebuntisSessionInformation {
+    sessionId: string
+    personType: number
+    personId: number
+}
+
+export default class Webuntis {
+    school: string
+    schoolbase64: string
+    username: string
+    password: string
+    domain: string
+    baseUrl: string
+    id: string
+
+    request: AxiosInstance
+    sessionInformation: WebuntisSessionInformation
+    cookies: string[]
+
+    constructor(
+        school: string,
+        username: string,
+        password: string,
+        domain: string
+    ) {
+        this.school = school
+        this.schoolbase64 = "_" + Buffer.from(school).toString("base64")
+        this.username = username
+        this.password = password
+        this.domain = domain
+        this.baseUrl = `https://${domain}/`
+        this.id = "test"
+
+        this.cookies = []
+        this.request = Axios.create({
+            baseURL: this.baseUrl,
+            maxRedirects: 0,
+            validateStatus: function(status) {
+                return status >= 200 && status < 303 // default
+            }
+        })
+    }
+
+    async login(): Promise<WebuntisSessionInformation> {
+        const response = await this.request({
+            method: "POST",
+            url: `/WebUntis/jsonrpc.do?school=${this.school}`,
+            data: {
+                id: this.id,
+                method: "authenticate",
+                params: {
+                    user: this.username,
+                    password: this.password,
+                    client: this.id
+                },
+                jsonrpc: "2.0"
+            }
+        })
+
+        if (typeof response.data !== "object")
+            throw new Error("Failed to parse server response.")
+        if (!response.data.result)
+            throw new Error("Failed to login. " + JSON.stringify(response.data))
+        if (response.data.result.code)
+            throw new Error(
+                "Login returned error code: " + response.data.result.code
+            )
+        if (!response.data.result.sessionId)
+            throw new Error("Failed to login. No session id.")
+
+        this.sessionInformation = response.data.result
+
+        return this.sessionInformation
+    }
+
+    async getAbsences(start: number, end: number): Promise<any[]> {
+        const url = `/WebUntis/api/classreg/absences/students?startDate=${start}&endDate=${end}&studentId=${
+            this.sessionInformation.personId
+        }&excuseStatusId=-1`
+
+        const response = await this.request({
+            method: "GET",
+            url,
+            headers: {
+                Cookie: this.buildCookies()
+            }
+        })
+
+        return response.data.data.absences
+    }
+
+    async getTimetableAt(date: number) {
+        const response = await this.request({
+            url: `/WebUntis/jsonrpc.do?school=${this.school}`,
+            method: "POST",
+            headers: {
+                Cookie: this.buildCookies()
+            },
+            data: {
+                id: this.id,
+                method: "getTimetable",
+                params: {
+                    id: this.sessionInformation.personId,
+                    type: this.sessionInformation.personType,
+                    startDate: date,
+                    endDate: date
+                },
+                jsonrpc: "2.0"
+            }
+        })
+
+        if (!response.data.result) throw new Error("Server didn't returned any result.");
+        if (response.data.result.code) throw new Error("Server returned error code: " + response.data.result.code);
+        return response.data.result;
+    }
+
+    async getTimetableForRange(start: number, end: number){
+        const response = await this.request({
+            url: `/WebUntis/jsonrpc.do?school=${this.school}`,
+            method: "POST",
+            headers: {
+                Cookie: this.buildCookies()
+            },
+            data: {
+                id: this.id,
+                method: "getTimetable",
+                params: {
+                    id: this.sessionInformation.personId,
+                    type: this.sessionInformation.personType,
+                    startDate: start,
+                    endDate: end
+                },
+                jsonrpc: "2.0"
+            }
+        })
+
+        if (!response.data.result) throw new Error("Server didn't returned any result.");
+        if (response.data.result.code) throw new Error("Server returned error code: " + response.data.result.code);
+
+        return response.data.result;
+    }
+
+    async getSubjects(){
+        const response = await this.request({
+            method: "POST",
+            url: `/WebUntis/jsonrpc.do?school=${this.school}`,
+            headers: {
+                Cookie: this.buildCookies()
+            },
+            data: {
+                id: this.id,
+                method: "getSubjects",
+                params: {},
+                jsonrpc: "2.0"
+            }
+        })
+
+        if (!response.data.result) throw new Error("Server didn't returned any result.");
+        if (response.data.result.code) throw new Error("Server returned error code: " + response.data.result.code);
+
+        return response.data.result;
+    }
+
+    buildCookies(): string {
+        let cookies = []
+        cookies.push(
+            CookieBuilder.serialize(
+                "JSESSIONID",
+                this.sessionInformation.sessionId
+            )
+        )
+        cookies.push(CookieBuilder.serialize("schoolname", this.schoolbase64))
+        return cookies.join("; ")
+    }
+}
