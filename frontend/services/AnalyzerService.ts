5a6eaa6f9ae7a033b9bd07ee0655ec728986ac35
Tim Untersberger <timuntersberger2@gmail.com>
2019-03-25 10:46:17 +0100

    change baseurl

--- frontend/services/AnalyzerService.ts
+++ frontend/services/AnalyzerService.ts
@@ -2,11 +2,9 @@ import Axios from "axios"
 
 export default {
     async analyze(username: string, password: string) {
-        let baseUrl = ""
+        let baseUrl = "http://localhost:8000"
         if (process.env.NODE_ENV === "production") {
-            baseUrl = "http://www.baaka.io:8000"
-        } else {
-            baseUrl = "http://localhost:8000"
+            baseUrl = "http://104.248.40.100:8000"
         }
         const response = await Axios.get(
             `${baseUrl}?username=${username}&password=${password}`
