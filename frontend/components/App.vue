e373d0d6917746557c1caca8e111b00b92464f99
Tim Untersberger <timuntersberger2@gmail.com>
2019-05-09 16:23:34 +0200

    .:

--- frontend/components/App.vue
+++ frontend/components/App.vue
@@ -1,37 +1,54 @@
 <template>
   <div id="app">
-    <v-snackbar v-model="isSnackbarVisible">
-      {{result.error}}
-      <v-btn color="primary" flat @click="isSnackbarVisible = false">Close</v-btn>
-    </v-snackbar>
-    <v-form @submit="login" class="form" v-if="!this.isAnalyzed && !this.isLoading">
-      <h1>Webuntis Analyzer</h1>
-      <v-text-field v-model="username" label="Username" placeholder="Username" required></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="password"
-        placeholder="Password"
-        required
-        type="password"
-      ></v-text-field>
-      <v-btn type="submit" color="blue">login</v-btn>
-    </v-form>
-    <v-progress-circular v-else-if="this.isLoading" indeterminate color="primary"></v-progress-circular>
-    <div class="result" v-else>
-      <h1>Total hours missed {{result.total}}</h1>
-      <h3>{{result.unexcused_absences_count}} unexcused Absences</h3>
-      <h2>Attendence</h2>
-      <div class="result-subjects">
-        <v-card class="card" v-for="info in result.infoPerSubject">
-          <h3>{{info.subject.name}}</h3>
-          <p></p>
-          <v-progress-circular
-            :value="100 - info.percentage"
-            :size="100"
-            color="blue-grey"
-          >{{100 - info.percentage}}</v-progress-circular>
-          <p>{{info.count}} hours missed</p>
-        </v-card>
+    <v-toolbar dense flat>
+      <v-toolbar-items>
+        <v-btn flat @click="analyze()">Analyze</v-btn>
+        <v-btn flat @click="showLeaderboard()">Leaderboard</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <div class="content">
+      <v-snackbar v-model="isSnackbarVisible">
+        {{result.error}}
+        <v-btn color="primary" flat @click="isSnackbarVisible = false">Close</v-btn>
+      </v-snackbar>
+      <v-form
+        @submit="login"
+        class="form"
+        v-if="!this.isLeaderboardVisible && !this.isAnalyzed && !this.isLoading"
+      >
+        <h1>Webuntis Analyzer</h1>
+        <v-text-field v-model="username" label="Username" placeholder="Username" required></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="password"
+          placeholder="Password"
+          required
+          type="password"
+        ></v-text-field>
+        <v-text-field v-model="domain" label="Domain" required></v-text-field>
+        <v-text-field v-model="school" label="School" required></v-text-field>
+        <v-btn type="submit" color="blue">login</v-btn>
+      </v-form>
+      <v-progress-circular v-else-if="this.isLoading" indeterminate color="primary"></v-progress-circular>
+      <div v-else-if="isLeaderboardVisible">leaderboard</div>
+      <div class="result" v-else>
+        <h1>Total hours missed {{result.total}}</h1>
+        <h3
+          v-if="result.unexcused_absences_count"
+        >{{result.unexcused_absences_count}} unexcused Absences</h3>
+        <h2>Attendence</h2>
+        <div class="result-subjects">
+          <v-card class="card" v-for="info in result.infoPerSubject">
+            <h3>{{info.subject.name}}</h3>
+            <p></p>
+            <v-progress-circular
+              :value="100 - info.percentage"
+              :size="100"
+              color="blue-grey"
+            >{{100 - info.percentage}}</v-progress-circular>
+            <p>{{info.count}} hours missed</p>
+          </v-card>
+        </div>
       </div>
     </div>
   </div>
@@ -44,24 +61,43 @@ export default {
   name: "app",
   data() {
     return {
+      isLeaderboardVisible: false,
       isSnackbarVisible: false,
       isLoading: false,
       isAnalyzed: false,
+      domain: "mese.webuntis.com",
+      school: "htbla linz leonding",
       username: "",
       password: "",
       result: {}
     };
   },
   methods: {
+    analyze() {
+      this.isLeaderboardVisible = false;
+      this.isAnalyzed = false;
+      this.result = {};
+    },
+    showLeaderboard() {
+      this.isAnalyzed = false;
+      this.result = {};
+      this.isLeaderboardVisible = true;
+    },
     login() {
       this.isLoading = true;
-      AnalyzerService.analyze(this.username, this.password).then(result => {
+      AnalyzerService.analyze(
+        this.username,
+        this.password,
+        this.school,
+        this.domain
+      ).then(result => {
         this.result = result;
         if (!result.error) {
           this.isAnalyzed = true;
         } else {
           this.isSnackbarVisible = true;
         }
+        this.password = "";
         this.isLoading = false;
       });
     }
@@ -73,6 +109,8 @@ export default {
 #app {
   font-family: Roboto, "Courier New", Courier, monospace;
   height: 100vh;
+}
+.content {
   padding: 30px 200px;
   display: flex;
   justify-content: center;
--- frontend/services/AnalyzerService.ts
+++ frontend/services/AnalyzerService.ts
@@ -1,14 +1,21 @@
 import Axios from "axios"
 
 export default {
-  async analyze(username: string, password: string) {
-    let baseUrl = "http://localhost:8080"
-    if (process.env.NODE_ENV === "production") {
-      baseUrl = "http://webuntis-analyzer.baaka.io"
+    async analyze(
+        username: string,
+        password: string,
+        school: string,
+        domain: string
+    ) {
+        let baseUrl = "http://localhost:8080"
+        if (process.env.NODE_ENV === "production") {
+            baseUrl = "http://webuntis-analyzer.baaka.io"
+        }
+        const response = await Axios.get(
+            `${baseUrl}?username=${username}&password=${btoa(
+                password
+            )}&school=${encodeURI(school)}&domain=${encodeURI(domain)}`
+        )
+        return response.data
     }
-    const response = await Axios.get(
-      `${baseUrl}?username=${username}&password=${btoa(password)}`
-    )
-    return response.data
-  }
 }
